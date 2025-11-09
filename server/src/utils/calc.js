6f67ce50c288e406b3d91fcd556fad5f215c197a
msomea <msomearaphael@gmail.com>
2025-11-09 12:01:27 +0300

    Add Button component

--- server/src/utils/calc.js
+++ server/src/utils/calc.js
@@ -2,7 +2,7 @@ function add(a, b) { return a + b; }
 module.exports = { add };
 
 // server/tests/unit/calc.test.js
-const { add } = require("../../src/utils/calc");
+const { add } = require("./calc");
 
 test("adds 2 numbers", () => {
   expect(add(2, 3)).toBe(5);
